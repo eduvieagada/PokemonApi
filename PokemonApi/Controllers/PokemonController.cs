1687089d6f2d58fd9aca1f3c80ced1d723e66e4c
eduvie agada <eduvieagada@gmail.com>
2021-10-13 18:27:00 +0100

    added pokemon endpoints

--- PokemonApi/Controllers/PokemonController.cs
+++ PokemonApi/Controllers/PokemonController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PokemonApi.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +12,39 @@ namespace PokemonApi.Controllers
     [Route("[controller]")]
     public class PokemonController : ControllerBase
     {
+        private readonly ILogger<PokemonController> _logger;
+        private readonly IPokemonService _pokemonService;
+
+        public PokemonController(ILogger<PokemonController> logger, IPokemonService pokemonService)
+        {
+            _logger = logger;
+            _pokemonService = pokemonService;
+        }
+
+        [HttpGet("{pokemonName}")]
+        public async Task<IActionResult> GetPokemon(string pokemonName)
+        {
+            if (string.IsNullOrWhiteSpace(pokemonName))
+            {
+                return BadRequest($"invalid pokemon name");
+            }
+
+            var pokemon = await _pokemonService.FetchPokemonData(pokemonName);
+
+            return Ok(pokemon);
+        }
+
+        [HttpGet("/translated/{pokemonName}")]
+        public async Task<IActionResult> GetPokemonTranslated(string pokemonName)
+        {
+            if (string.IsNullOrWhiteSpace(pokemonName))
+            {
+                return BadRequest($"invalid pokemon name");
+            }
+
+            var pokemon = await _pokemonService.FetchTranslatedPokemonData(pokemonName);
+
+            return Ok(pokemon);
+        }
     }
 }
