8ff2a0f6fe93c4a7dc7a8b95331feb6e3be3e336
eduvie agada <eduvieagada@gmail.com>
2021-10-13 20:29:32 +0100

    Added poke api integration

--- Pokemon.Api.PokeApi/PokeApiService.cs
+++ Pokemon.Api.PokeApi/PokeApiService.cs
@@ -2,14 +2,40 @@
 using System;
 using System.Threading.Tasks;
 using PokemonApi.Core.Dto;
+using System.Net.Http;
+using System.Net.Http.Json;
+using PokemonApi.Core.Exceptions;
+using System.Linq;
 
 namespace PokemonApi.PokeApi
 {
     public class PokeApiService : IPokemonDataSource
     {
-        public Task<Pokemon> FetchPokemon(string pokemonName)
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly string baseUrl;
+
+        public PokeApiService(IHttpClientFactory httpClientFactory, string baseUrl)
         {
-            throw new NotImplementedException();
+            this.httpClientFactory = httpClientFactory;
+            this.baseUrl = baseUrl;
+        }
+
+        public async Task<Core.Dto.Pokemon> FetchPokemon(string pokemonName)
+        {
+            var client = httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(baseUrl);
+
+            var response = await client.GetFromJsonAsync<PokeApiResponse>($"/pokemon-species/{pokemonName}");
+
+            if (response is null) throw new PokemonNotFoundException(pokemonName);
+
+            return new Core.Dto.Pokemon
+            {
+                Name = response.name,
+                Description = response.flavor_text_entries.FirstOrDefault()?.flavor_text,
+                Habitat = response.habitat?.name,
+                IsLegendary = response.is_legendary
+            };
         }
     }
 }
--- PokemonApi/Dependencies/PokemonDataSourceDependencyResolver.cs
+++ PokemonApi/Dependencies/PokemonDataSourceDependencyResolver.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PokemonApi.Core;
+using PokemonApi.PokeApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace PokemonApi.Dependencies
 {
     public static class PokemonDataSourceDependencyResolver
     {
+        public static IServiceCollection AddPokeApi(this IServiceCollection services, IConfiguration configuration)
+        {
+            var baseUrl = configuration["PokeApi"] ??
+                throw new InvalidOperationException("Unable to instantiate Pokemon data source)");
+
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var httpClientFactory = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>();
+
+            services.AddScoped<IPokemonDataSource>(_ => new PokeApiService(httpClientFactory, baseUrl));
+
+            return services;
+        }
     }
 }
